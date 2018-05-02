aaab2a65e8c4385ab82eef90bc83f70574d501e1
Mike Smith <grimbough@gmail.com>
2018-05-02 18:24:34 +0200

    modifed bwa example

--- exercises/bwa/bwa_batch.sh
+++ exercises/bwa/bwa_batch.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-#SBATCH --mem 150
-#SBATCH --nodes 8
+#SBATCH -t 00:15:00
+#SBATCH --mem=150
+#SBATCH --nodes=1
+
+## copy data to /scratch and change directory
+cp reference.fa reads.txt $SCRATCHDIR && cd $SCRATCHDIR
 
 ## create an index
 bwa index reference.fa
@@ -10,3 +14,6 @@ bwa aln -I reference.fa reads.txt > out.sai
 
 ## create SAM file
 bwa samse reference.fa out.sai reads.txt > out.sam
+
+## copy results back
+cp out.sam $HOME/embl_hpc/exercises/bwa/
